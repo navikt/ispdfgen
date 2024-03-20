e94c91b5795e069eeb3a0884085f6082b808d5ef
Anders Rognstad <79838644+andersrognstad@users.noreply.github.com>
2024-03-20 13:26:39 +0100

    IS-2209: Setup app (#1)
    * IS-2209: Setup app
    
    * increase jdk memory

---
+++ run_development_windows.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+CURRENT_PATH=$(pwd)
+
+winpty docker pull ghcr.io/navikt/pdfgen:2.0.28
+winpty docker run \
+        -v "/$CURRENT_PATH/templates:/app/templates" \
+        -v "/$CURRENT_PATH/fonts:/app/fonts" \
+        -v "/$CURRENT_PATH/data:/app/data" \
+        -v "/$CURRENT_PATH/resources:/app/resources" \
+        -p 8080:8080 \
+        -e DISABLE_PDF_GET=false \
+        -e JDK_JAVA_OPTIONS \
+        -it \
+        --rm \
+        ghcr.io/navikt/pdfgen:2.0.23
+
