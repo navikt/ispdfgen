442e9c47f195bd6987dffc1d90b68990e3b4a315
geir-waagboe <geir.arne.waagbo@nav.no>
2026-07-13 15:58:30 +0200

    IS-3968: Gradle-scaffold

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        maven {
+            url = uri("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
+        }
+        mavenLocal()
+    }
+    versionCatalogs {
+        create("ktorLibs").from("io.ktor:ktor-version-catalog:3.5.1")
+    }
+}
+
+rootProject.name = "ispdfgen"
